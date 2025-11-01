a451ab87ae3881d5405da7e1c7ecf84027e44258
Ananya Pathak <anyalko043@gmail.com>
2025-11-01 22:36:48 +0530

    feat: add Main.java as entry point to launch JavaFX Space Game

--- src/com/spacegame/Main.java
+++ src/com/spacegame/Main.java
@@ -1,9 +1,31 @@
 package com.spacegame;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.stage.Stage;
 
-        }
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Group root = new Group();
+        Canvas canvas = new Canvas(800, 600);
+        root.getChildren().add(canvas);
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("2D Space Game");
+        stage.setResizable(false);
+        stage.show();
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        new Game(gc, scene);
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
