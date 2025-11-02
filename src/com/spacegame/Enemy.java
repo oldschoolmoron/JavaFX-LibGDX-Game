e561caf9a0afa948e9f952d2be08cc1db4c2a95b
Ananya Pathak <anyalko043@gmail.com>
2025-11-02 23:49:46 +0530

    feat: add basic Enemy class with position and movement logic

--- src/com/spacegame/Enemy.java
+++ src/com/spacegame/Enemy.java
@@ -1,4 +1,27 @@
 package com.spacegame;
 
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
 public class Enemy {
+
+    private double x;
+    private double y;
+    private double width;
+    private double height;
+    private double speed;
+
+    // constructor gives each enemy a position and speed
+    public Enemy(double x, double y) {
+        this.x = x;
+        this.y = y;
+        this.width = 40;
+        this.height = 20;
+        this.speed = 2; // moves down slowly
+    }
+
+    // update position (moves enemy down the screen)
+    public void update() {
+        y += speed;
+    }
 }
