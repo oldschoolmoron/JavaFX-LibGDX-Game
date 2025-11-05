d54af2b266f2cc88ea7d3393d38269265dc170cd
Ananya Pathak <anyalko043@gmail.com>
2025-11-05 22:02:52 +0530

    feat: add Bullet class with basic movement and rendering

--- src/com/spacegame/Bullet.java
+++ src/com/spacegame/Bullet.java
@@ -1,4 +1,42 @@
 package com.spacegame;
 
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
 public class Bullet {
+
+    private double x;
+    private double y;
+    private double speed = 7;
+    private boolean active = true;
+
+    public Bullet(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void update() {
+        y -= speed;
+
+        if (y < 0) {
+            active = false;
+        }
+    }
+
+    public void draw(GraphicsContext gc) {
+        gc.setFill(Color.YELLOW);
+        gc.fillOval(x, y, 5, 10);
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
 }
